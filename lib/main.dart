2d11536777eab90141d568bf859a5b33e6eacdcc
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2023-11-13 14:44:35 +0700

    ini sidebar ma homecard ygy

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lookup_app/screens/signup_screen.dart';
+import 'package:lookup_app/ui/homecard.dart';
+import 'package:lookup_app/ui/sidebar.dart';
 import 'package:lookup_app/utils/colors.dart';
+import 'package:lookup_app/ui/homecard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-      apiKey: "AIzaSyCqwd5L11mayRTMNVxj36OqTVnvu970efE",
-      appId: "1:4338278854:web:061fcc0ff1756d424752c7",
-      messagingSenderId: "4338278854",
-      projectId: "lookup-app-3271c",
-      storageBucket: "lookup-app-3271c.appspot.com",
-    ));
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCqwd5L11mayRTMNVxj36OqTVnvu970efE",
+        appId: "1:4338278854:web:061fcc0ff1756d424752c7",
+        messagingSenderId: "4338278854",
+        projectId: "lookup-app-3271c",
+        storageBucket: "lookup-app-3271c.appspot.com",
+      ),
+    );
   }
   await Firebase.initializeApp();
 
@@ -22,7 +26,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   // This widget is the root of your application.
   @override
@@ -33,11 +37,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      // home: const ResponsiveLayout(
-      // mobileScreenLayout: MobileScreenLayout(),
-      //  webScreenLayout: WebScreenLayout(),
-      // ),
-      home: const SignupScreen(),
+      home: HomeCard(),
+    );
+    
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: Center(
+        child: const Text('Halaman Utama'),
+      ),
+      drawer: Sidebar(), // Sidebar sebagai drawer
     );
   }
 }
