58c2331cbbcfc158b62a2724a4fca6b3279d8f36
raudhotinekaputri <raudhotinekaputri@gmail.com>
2023-11-13 14:48:13 +0700

    sign up aput

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lookup_app/screens/signup_screen.dart';
+import 'package:lookup_app/ui/signup_page.dart';
 import 'package:lookup_app/utils/colors.dart';
 
 void main() async {
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
       // mobileScreenLayout: MobileScreenLayout(),
       //  webScreenLayout: WebScreenLayout(),
       // ),
-      home: const SignupScreen(),
+      home: const SignUpPage(),
     );
   }
 }
