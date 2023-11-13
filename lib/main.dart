c11a6c8f848c2bc7b8d7d8415c17e628f553ade0
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2023-11-13 18:06:26 +0700

    homecard blm jadi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lookup_app/screens/signup_screen.dart';
-import 'package:lookup_app/ui/signup_page.dart';
 import 'package:lookup_app/utils/colors.dart';
 import 'package:lookup_app/ui/homecard.dart';
 
@@ -40,7 +39,7 @@ class MyApp extends StatelessWidget {
       // mobileScreenLayout: MobileScreenLayout(),
       //  webScreenLayout: WebScreenLayout(),
       // ),
-      home: const SignUpPage(),
+      home: HomeCard(),
     );
   }
 }
