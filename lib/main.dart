7e9b48acfb86b3155fb59c5385d4705a4ad70e87
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2023-11-15 18:39:46 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       // mobileScreenLayout: MobileScreenLayout(),
       //  webScreenLayout: WebScreenLayout(),
       // ),
-      home: const SignUpPage(),
+      home: HomeCard(),
     );
   }
 }
--- lib/ui/homecard.dart
+++ lib/ui/homecard.dart
@@ -20,15 +20,8 @@ class HomeCard extends StatelessWidget {
               ),
             )
           ],
-        )),
-      // body: SafeArea(
-      //   child: Column(children: [
-      //     SizedBox(height: 96),
-      //     Padding(padding:EdgeInsets.symmetric(vertical: 10), child:CardContainer() ,),
-      //     SizedBox(height: 16.0),
-      //       CardContainer(),
-      //   ],)
-      // ),
+        )
+      ),
     );
   }
 }
@@ -37,31 +30,52 @@ class CardContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Padding(padding: EdgeInsets.all(16), 
-      child: Card(
-        elevation: 5.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Container(
-        width: 300,
-        height: 400,
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.0),
-          color: Colors.white,
-          border: Border.all(color: Colors.white),
-        ),
-        child: // Tambahkan konten di sini
-          Center(
-            child: Text(
-              "Card Content",
-              style: TextStyle(fontSize: 18.0),
-            ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Card(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
           ),
+          child: Stack(
+            children: [
+              Container(
+                width: 300,
+                height: 400,
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16.0),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.white),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Text(
+                        'Ditemukan',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Image.asset(
+                        'assets/google.png', // Ganti dengan path gambar Anda
+                        width: 100, // Sesuaikan lebar gambar
+                        height: 100, // Sesuaikan tinggi gambar
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      ),)
     );
   }
 }
-
