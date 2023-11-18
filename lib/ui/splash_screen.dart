458ba1be12351569d0bc83089a2fecf22122c11d
KhalimahMusaadah <khalimahmusaadah@gmail.com>
2023-11-18 20:19:24 +0700

    commit

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:lookup_app/ui/login_page.dart';
 import 'package:lookup_app/ui/homecard.dart';
-import 'package:lookup_app/ui/profile.dart';
 import 'package:lookup_app/ui/sidebar.dart';
 import 'package:lookup_app/ui/signup_page.dart';
 
@@ -25,7 +24,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
     return Timer(durasiSplash, () {
       //pindah ke halaman home
       Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
-        return Profile();
+        return SignUpPage();
       }));
     });
   }
