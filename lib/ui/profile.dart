2e4616db275da30bb762dc2c3c86943ae90e42a8
rizkythahatmaputri <rithakendal@gmail.com>
2023-11-30 00:14:29 +0700

    delete uid

--- lib/ui/profile.dart
+++ lib/ui/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({Key? key, required String uid});
+  const Profile({Key? key});
 
   @override
   Widget build(BuildContext context) {
